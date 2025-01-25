04ea68a1a53e0feab7c82b45fe908c89090100a4
ahmdaliielsayed <ahmdaliielsayed@gmail.com>
2025-01-25 17:52:35 +0200

    first commit

---
+++ app/src/main/java/com/example/eui/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.eui
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    // (X) main method
+    // lifecycle --> will show --> shown --> will be hidden --> hidden
+
+    var counter = 0
+
+    lateinit var btnRegister: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // inflation
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // initialization - inflation - not visible
+        Log.d("MainActivity --> lifecycle: ", "onCreate()")
+
+        if (savedInstanceState == null) {
+            Log.d("MainActivity --> lifecycle: ", "first enter")
+        } else {
+            Log.d("MainActivity --> lifecycle: ", "second time")
+            counter = savedInstanceState.getInt("COUNTER")
+            counter++
+            Log.d("MainActivity --> lifecycle: ", "second time & counter = $counter")
+        }
+
+        val textChange = findViewById<TextView>(R.id.text_change)
+//        val btnRegister = findViewById<Button>(R.id.btn_register)
+        btnRegister = findViewById(R.id.btn_register)
+
+        btnRegister.setOnClickListener {
+            textChange.text = getText(R.string.change_eui)
+            val intent = Intent(this, SecondActivity::class.java)
+            // key --> value
+            intent.putExtra("EMAIL", "ahmdaliielsayed@gmail.com")
+            intent.putExtra("MOBILE", "01063208399")
+            startActivity(intent)
+        }
+
+        val btnViewGoogle = findViewById<Button>(R.id.btnViewGoogle)
+        btnViewGoogle.setOnClickListener {
+            val uri = Uri.parse("tel:01063208399")
+            val intent = Intent(Intent.ACTION_DIAL, uri)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // will be visible --> start (thread - animation - sound)
+        Log.d("MainActivity --> lifecycle: ", "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // activity visible and user can interact
+        Log.d("MainActivity --> lifecycle: ", "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // partially invisible
+        Log.d("MainActivity --> lifecycle: ", "onPause()")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // key --> value
+        outState.putInt("COUNTER", counter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // is NOT visible
+        Log.d("MainActivity --> lifecycle: ", "onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity --> lifecycle: ", "onRestart()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity --> lifecycle: ", "onDestroy()")
+    }
+}
---
+++ app/src/main/java/com/example/eui/SecondActivity.kt
@@ -0,0 +1,31 @@
+package com.example.eui
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var myData: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        myData = findViewById(R.id.textView)
+
+        val email = intent.getStringExtra("EMAIL") // ahmdaliielsayed@gmail.com
+        val mobile = intent.getStringExtra("MOBILE") // 01063208399
+
+        myData.text = "Email: $email \nMobile: $mobile"
+    }
+}
